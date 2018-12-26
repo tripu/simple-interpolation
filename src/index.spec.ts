c3dcf70d3aa646630ef1a9d93f6c63fbb1d9166d
Dmytro Paduchak <dmytro.paduchak@gmail.com>
2018-12-26 23:03:32 +0200

    p

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,11 @@
 import { describe, it } from "mocha";
-
 import { expect } from "chai";
+
+import { single } from './index';
 
 describe("index.ts", () => {
   it("should do file", () => {
+    console.log(single([{ x: 1, v: 1}, { x: 2, v: 2 }]));
     expect(true).to.equal(true);
   });
 });
