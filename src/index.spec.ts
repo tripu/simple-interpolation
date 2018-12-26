9f067261234034baf27aed10764922811b2159dd
Dmytro Paduchak <Dmytro_Paduchak@epam.com>
2018-12-26 14:47:27 +0200

    start module dependencies and logic

---
+++ src/index.spec.ts
@@ -0,0 +1,9 @@
+import { describe, it } from "mocha";
+
+import { expect } from "chai";
+
+describe("index.ts", () => {
+  it("should do file", () => {
+    expect(true).to.equal(true);
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+
+
+
+/**
+* @method: Returns single interpolation method.
+* @param {string}
+* @return {void}
+*/
+export function single (arr: any) : string {
+  return 'test';
+}
