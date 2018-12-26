e93a1793a0fc79885dfd541ea2af07ed5bd5a802
Dmytro Paduchak <dmytro.paduchak@gmail.com>
2018-12-26 22:36:28 +0200

    add test build tslint

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
-
-
-
 /**
-* @method: Returns single interpolation method.
-* @param {string}
-* @return {void}
-*/
-export function single (arr: any) : string {
-  return 'test';
+ * @method: Returns single interpolation method.
+ * @param {string}
+ * @return {void}
+ */
+export function single(arr: any): string {
+  return "test";
 }
