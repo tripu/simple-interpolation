eabc7fd2e9960507079f676f1aa0435d69182e39
Dmytro Paduchak <Dmytro_Paduchak@epam.com>
2019-01-02 12:56:03 +0200

    start double interpolation

--- src/double.ts
+++ src/double.ts
@@ -1,5 +1,25 @@
-export function double(): any {
-  return (): number => {
+/** Input Point Structure Interface */
+interface Point {
+  x: number;
+  y: number;
+  v: number;
+}
+
+/** Search Point Cursor Type */
+type Cursor<T, U = { [K in keyof T]: number }> = U;
+
+/**
+ * Dounble interpolation store
+ * 
+ * @param {string} points interpolation matrix data
+ * @return {void} interpolation execut method
+ */
+export function double(points: Point[]): <T>(cursor: Cursor<T>) => number {
+  if (points.length <= 1) {
+    throw "Can't calculate double interpolation, please provide points";
+  }
+
+  return <T>(cursor: Cursor<T>): number => {
     return 1;
   };
 }
